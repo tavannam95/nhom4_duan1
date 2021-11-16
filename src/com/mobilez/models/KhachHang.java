d187a7fe45d46b83f7013410a583ef655361fa10
giangPham27 <94352409+giangPham27@users.noreply.github.com>
2021-11-16 15:19:55 +0700

    commit class KhachHang

---
+++ src/com/mobilez/models/KhachHang.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mobilez.models;
+
+/**
+ *
+ * @author Admin
+ */
+public class KhachHang {
+//    - Mã khách hàng (MaKH)
+//- Tên khách hàng (TenKH)
+//- Điạ chỉ (DiaChi)
+//- Điện thoại (Phone)
+    private String MaKH, TenKH, diaChi, soDienThoai ;
+
+    public KhachHang(String MaKH, String TenKH, String diaChi, String soDienThoai) {
+        this.MaKH = MaKH;
+        this.TenKH = TenKH;
+        this.diaChi = diaChi;
+        this.soDienThoai = soDienThoai;
+    }
+
+    public KhachHang() {
+    }
+
+    @Override
+    public String toString() {
+        return "KhachHang{" + "MaKH=" + MaKH + '}';
+    }
+
+   
+}
