b1954061208f930de394b3a0151b195910f7822b
dgvson <duongson9c@gmail.com>
2021-11-16 15:33:08 +0700

    add class HSX

---
+++ src/com/mobilez/models/HangSanXuat.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mobilez.models;
+
+/**
+ *
+ * @author duong
+ */
+public class HangSanXuat {
+    private String maHSX, tenHSX;
+
+    public HangSanXuat() {
+    }
+
+    public HangSanXuat(String maHSX, String tenHSX) {
+        this.maHSX = maHSX;
+        this.tenHSX = tenHSX;
+    }
+
+    public String getMaHSX() {
+        return maHSX;
+    }
+
+    public void setMaHSX(String maHSX) {
+        this.maHSX = maHSX;
+    }
+
+    public String getTenHSX() {
+        return tenHSX;
+    }
+
+    public void setTenHSX(String tenHSX) {
+        this.tenHSX = tenHSX;
+    }
+
+    @Override
+    public String toString() {
+        return maHSX;
+    }
+    
+}
