11266db854e70eb8717928703d57269d7ecd2531
giangPham27 <94352409+giangPham27@users.noreply.github.com>
2021-11-17 14:38:03 +0700

    giangpc_ClassKH

---
+++ src/com/mobilez/models/KhachHang.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mobilez.models;
+
+/**
+ *
+ * @author Admin
+ */
+public class KhachHang {
+    private String MaKH, TenKH, diaChi, soDienThoai ;
+
+    public KhachHang(String MaKH, String TenKH, String diaChi, String soDienThoai) {
+        this.MaKH = MaKH;
+        this.TenKH = TenKH;
+        this.diaChi = diaChi;
+        this.soDienThoai = soDienThoai;
+    }
+
+    public KhachHang() {
+    }
+     @Override
+    public String toString() {
+        return this.MaKH;
+    }
+}
