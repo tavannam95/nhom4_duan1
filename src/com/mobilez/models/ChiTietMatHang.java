ea97af8c16c3f2b418912e0ebaa066adb96ecb62
dgvson <duongson9c@gmail.com>
2021-11-17 13:26:05 +0700

    add XImage, ChiTietMatHang

---
+++ src/com/mobilez/models/ChiTietMatHang.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mobilez.models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author duong
+ */
+public class ChiTietMatHang {
+
+    private String ram;
+    private String dungluong;
+    private String mausac;
+    private Date ngaycapnhat;
+    private String maQG;
+    private String maMH;
+    private double giamua;
+    private double giaban;
+
+    public ChiTietMatHang() {
+    }
+
+    public ChiTietMatHang(String ram, String dungluong, String mausac, Date ngaycapnhat, String maQG, String maMH, double giamua, double giaban) {
+        this.ram = ram;
+        this.dungluong = dungluong;
+        this.mausac = mausac;
+        this.ngaycapnhat = ngaycapnhat;
+        this.maQG = maQG;
+        this.maMH = maMH;
+        this.giamua = giamua;
+        this.giaban = giaban;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    public String getDungluong() {
+        return dungluong;
+    }
+
+    public void setDungluong(String dungluong) {
+        this.dungluong = dungluong;
+    }
+
+    public String getMausac() {
+        return mausac;
+    }
+
+    public void setMausac(String mausac) {
+        this.mausac = mausac;
+    }
+
+    public Date getNgaycapnhat() {
+        return ngaycapnhat;
+    }
+
+    public void setNgaycapnhat(Date ngaycapnhat) {
+        this.ngaycapnhat = ngaycapnhat;
+    }
+
+    public String getMaQG() {
+        return maQG;
+    }
+
+    public void setMaQG(String maQG) {
+        this.maQG = maQG;
+    }
+
+    public String getMaMH() {
+        return maMH;
+    }
+
+    public void setMaMH(String maMH) {
+        this.maMH = maMH;
+    }
+
+    public double getGiamua() {
+        return giamua;
+    }
+
+    public void setGiamua(double giamua) {
+        this.giamua = giamua;
+    }
+
+    public double getGiaban() {
+        return giaban;
+    }
+
+    public void setGiaban(double giaban) {
+        this.giaban = giaban;
+    }
+
+    @Override
+    public String toString() {
+        return "ChiTietMatHang{" + "maQG=" + maQG + ", maMH=" + maMH + '}';
+    }
+    
+}
