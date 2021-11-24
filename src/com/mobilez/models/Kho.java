8b6a04632748a1a4e9afe683548597e1a664d0f9
tavannam95 <namtvph13082@fpt.edu.vn>
2021-11-24 16:58:09 +0700

    namtv Hoa Don Ban Si

---
+++ src/com/mobilez/models/Kho.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mobilez.models;
+
+/**
+ *
+ * @author uhtku
+ */
+public class Kho {
+
+    String maK, tenK, diaChi;
+
+    public Kho() {
+    }
+
+    public Kho(String maK, String tenK, String diaChi) {
+        this.maK = maK;
+        this.tenK = tenK;
+        this.diaChi = diaChi;
+    }
+
+    @Override
+    public String toString() {
+        return this.tenK; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public String getMaK() {
+        return maK;
+    }
+
+    public void setMaK(String maK) {
+        this.maK = maK;
+    }
+
+    public String getTenK() {
+        return tenK;
+    }
+
+    public void setTenK(String tenK) {
+        this.tenK = tenK;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+}
